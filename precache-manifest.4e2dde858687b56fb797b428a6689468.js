c13063db9e58a4eafa2491d46801326c4ebe9bc4
zengrui <zengr666@163.com>
2021-03-13 10:54:11 +0800

    deploy

---
+++ precache-manifest.4e2dde858687b56fb797b428a6689468.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdca5af53310877b24a4",
+    "url": "/MyDaoDao/css/app.d07cfc88.css"
+  },
+  {
+    "revision": "706e2fc4e91b4c9c0992",
+    "url": "/MyDaoDao/css/chunk-vendors.c331b2d6.css"
+  },
+  {
+    "revision": "fa84f1eec57263a651b793c62773bdc6",
+    "url": "/MyDaoDao/img/qrcode.fa84f1ee.png"
+  },
+  {
+    "revision": "bf1a065d160fd9c591f22d2c921961da",
+    "url": "/MyDaoDao/index.html"
+  },
+  {
+    "revision": "bdca5af53310877b24a4",
+    "url": "/MyDaoDao/js/app.a805c7f3.js"
+  },
+  {
+    "revision": "706e2fc4e91b4c9c0992",
+    "url": "/MyDaoDao/js/chunk-vendors.af6b50f8.js"
+  },
+  {
+    "revision": "bd079fbbc6d959e5f2ab39009d6e3a68",
+    "url": "/MyDaoDao/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MyDaoDao/robots.txt"
+  }
+]);
